9e16cdb419762642725d64be7bc95233a08fb6b5
Tirthraj Barot <tirthrajbarot2394@gmail.com>
2017-07-11 16:51:17 +0530

    new data

---
+++ assets/js/ecma1.js
@@ -0,0 +1,9 @@
+function mainbutton(){
+	document.getElementById('mainbtn').style.backgroundColor="#2E6077";
+}
+function outa(){
+	document.getElementById('mainbtn').style.backgroundColor="#01a084";
+}
+function clickbtn(){
+	
+}
